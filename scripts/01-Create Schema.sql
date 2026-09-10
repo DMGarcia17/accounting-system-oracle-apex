c9f4d9fcb6a3b3284e8c6b79ad06a2c9407714bf
DMGarcia17 <37032838+DMGarcia17@users.noreply.github.com>
2026-09-09 21:47:03 -0600

    Environment ready.

---
+++ scripts/01-Create Schema.sql
@@ -0,0 +1,28 @@
+-- Ejecutar conectado como SYS con rol SYSDBA, dentro del PDB XEPDB1
+-- (verificá que estés en el contenedor correcto: SHOW CON_NAME; debe decir XEPDB1)
+
+ALTER SESSION SET CONTAINER = XEPDB1;
+
+-- Crear el usuario/schema de trabajo
+-- La contraseña se deja versionada debido a la conexión entre sistemas
+CREATE USER CONTA_SCHEMA IDENTIFIED BY "D5wvTHNBpv9vGwBYfpBr"
+  DEFAULT TABLESPACE USERS
+  TEMPORARY TABLESPACE TEMP
+  QUOTA UNLIMITED ON USERS;
+
+-- Privilegios básicos para desarrollo (crear tablas, vistas, procedimientos, etc.)
+GRANT CREATE SESSION TO CONTA_SCHEMA;
+GRANT CREATE TABLE TO CONTA_SCHEMA;
+GRANT CREATE VIEW TO CONTA_SCHEMA;
+GRANT CREATE SEQUENCE TO CONTA_SCHEMA;
+GRANT CREATE PROCEDURE TO CONTA_SCHEMA;
+GRANT CREATE TRIGGER TO CONTA_SCHEMA;
+GRANT CREATE SYNONYM TO CONTA_SCHEMA;
+
+-- Necesario más adelante para exponer el schema vía ORDS/APEX (job scheduling, etc.)
+GRANT CREATE JOB TO CONTA_SCHEMA;
+
+-- Confirmar que se creó correctamente
+SELECT username, account_status, default_tablespace
+FROM dba_users
+WHERE username = 'CONTA_SCHEMA';
